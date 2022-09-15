5a3f9842fc82d313b0df0ccd8df8fcadb4a4e4a0
Alihan Büyüköz <alihan.buyukoz@ceng.deu.edu.tr>
2022-09-15 13:27:08 +0300

    final

--- libs/middlewares/token.middleware.ts
+++ libs/middlewares/token.middleware.ts
@@ -10,41 +10,41 @@ export class TokenMiddleware implements NestMiddleware {
     const authJsonWebToken = req.headers.authorization;
 
     if (req.path !== '/api/login') {
-        // console.log("user yolu");
-         // login hariç tüm metodlarda token kotrolü yapıyoruz. çünkü token alabilmesi için login olması gerekiyor
-        if(!authJsonWebToken){ // tokeni yoksa
-            //console.log("tokeni yok");
+        console.log("user yolu");
+        //  login hariç tüm metodlarda token kotrolü yapıyoruz. çünkü token alabilmesi için login olması gerekiyor
+        if(!authJsonWebToken){ // tokeni yoksa exception fırlatacak
+            console.log("tokeni yok");
             next();
             throw new HttpException(
                 'Jwt could not found!',
                 HttpStatus.FORBIDDEN
             );
         }else{
-            // console.log("tokeni  var");
+            console.log("tokeni  var"); // tokeni varsa önceden hashlediğimiz bir tokenle uyuşmasına bakıp verify ediyoruz
             try {
                 const user = jwt.verify(authJsonWebToken.slice(7, authJsonWebToken.length),  // kendi ürettiğimiz bir token old. emin olmak için verify edeceğiz.
                 environment.jwtText,  // kendi ürettiğimiz bir token old. emin olmak için verify edeceğiz.
                 ); 
 
                 if(user){  // user varsa
-                    // console.log("user var");
+                    console.log("user var");
                     req['user'] = user;  // requeste bir user ekliyip onu da user bilgisine eşitliyor.  ileride guardlarda işe yarayacak
                     next();
                 }else{
-                    // console.log("user yok");
+                    console.log("user yok");
                     throw new HttpException(
                         'something went wrong!',
                         HttpStatus.GATEWAY_TIMEOUT,
                     );
                 }
             } catch (ex) {
-                // console.log("hata var");
+                console.log("hata var");
                 throw new HttpException(ex.message, HttpStatus.UNAUTHORIZED);
             }
         }
     } else {
  
-        // console.log("api yolu");
+        console.log("login yolu");
         next();
         return;
     }
